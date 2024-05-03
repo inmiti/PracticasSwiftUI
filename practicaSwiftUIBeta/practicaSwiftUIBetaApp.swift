020c06d09c98e9b62653ee4df6297bac31b63d5f
inmiti <inmabauvel@gmail.com>
2024-05-03 10:16:16 +0200

    Initial Commit

---
+++ practicaSwiftUIBeta/practicaSwiftUIBetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  practicaSwiftUIBetaApp.swift
+//  practicaSwiftUIBeta
+//
+//  Created by ibautista on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct practicaSwiftUIBetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
